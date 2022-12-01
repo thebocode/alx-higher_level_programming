8603f98c5f3a4fa378859de22cde28a626dd18c0
thebocode <bosunadebola@gmail.com>
2022-12-01 11:03:53 +0000

    import function of calculator

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Arguments:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value of (a + b)
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My subtraction function
+
+    Arguments:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value of (a - b)
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My multiplication function
+
+    Arguments:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value of (a * b)
+    """
+    return (a * b)
+
+def div(a, b):
+    """My division function
+
+    Arguments:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value of (a / b)
+    """
+    return int(a / b)
